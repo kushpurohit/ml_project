bbed2df5a4676c4699f1d594eeb92864ef5fefce
kushpurohit <kushpurohit104@gmail.com>
2024-07-18 02:37:18 +0530

    Predict Pipeline

--- src/Components/data_ingestion.py
+++ src/Components/data_ingestion.py
@@ -62,8 +62,11 @@ class DataIngestion:
 if __name__=="__main__":
     obj=DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
+
     data_transformation = DataTransformation()
-    train_arr, test_arr,_ = data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
     modelTrainer=ModelTrainer()
-    print(modelTrainer.initiate_model_training(train_arr, test_arr))
+    model_name,r2_score=modelTrainer.initiate_model_training(train_arr, test_arr)
+    print(model_name)
+    print(r2_score)
